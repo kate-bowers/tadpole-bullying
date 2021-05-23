e7c057a3a4baab38782888cfb23b0f69f067b0c5
Kate Bowers <kbowers405@gmail.com>
2021-05-23 18:54:49 -0400

    Refactored find_collisions from one giant file into different files.

---
+++ isRedundant.py
@@ -0,0 +1,43 @@
+# Kate Bowers
+# Levin Lab Tufts University
+# Program development started March 2021
+# Beckman Scholars - Tadpole Bullying
+# isRedundant.py -- identifies if a given collision is redundant/multiple-ID #TODO this is a bad desc
+
+from utilities import dist
+
+
+def is_redundant(collisions_so_far, curr_coll, wrong_distances):  # tells whether this is a redundant/multipleID WT collision
+    # curr_coll -- time, wt_num, wxy,
+    time = curr_coll[0]
+    identity = curr_coll[1]
+    xy = curr_coll[2]
+
+    '''all_collisions.append(
+        (float(time_of_smallest_dist) - float(def_start), #time corrected for when def shows up
+            disp, 
+            vel, 
+            False,
+            wt_num, 
+            wxy,
+            def_num)
+        )'''
+    for coll in collisions_so_far: # TODO i got rid of the print statements here dont forget
+        if (abs(time - coll[0]) <= 0.5):  # within half a second
+            if wrong_distances:
+                distance = dist(coll[5], xy) / 10
+            else:
+                distance = dist(coll[5], xy)
+
+            if (distance <= 3):  # within one mm idk lol
+                # print("YEEEET BAD BOYIIEEEE, ", distance, mins(time), mins(coll[0]), identity, coll[4])
+                return True
+
+            if (identity == coll[4]):
+                # print("MEEEEEEEEEMES LOL", mins(time), mins(coll[0]), identity, coll[4])
+                return True
+
+            # else:
+                # print("naurrrr \n but yeet dist was ", distance, mins(time), mins(coll[0]), identity, coll[4])
+
+    return False
