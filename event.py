787d786cb96767f8074391991412f924f6a55091
Kate Bowers <kbowers405@gmail.com>
2021-07-06 00:17:07 -0400

    replaced the time-based timeout with just ending after going out of proximity, and only record a collision if it is the first vvalid one within the first window of necessary distance

--- event.py
+++ event.py
@@ -4,8 +4,8 @@
 # Beckman Scholars - Tadpole Bullying
 # event.py -- class declaration for Event class
 
-from utilities import mins, PROXIMITY_DISTANCE, NECESSARY_DISTANCE, TOO_CLOSE
-
+from utilities import mins, dist, PROXIMITY_DISTANCE, NECESSARY_DISTANCE, TOO_CLOSE
+from isRedundant import is_redundant
 
 # TODO i want to get rid of timed_out as a concept but should just implement classes in general first
 class Event:
@@ -22,9 +22,10 @@ class Event:
         self.def_num = def_num
         self.wt_num = wt_num
         self.collision_window_open = False
-        # self.collision_
+        self.collision_found = False
 
     # collisionValid returns True if the event at its current state is a valid collision to record, False if not
+    # helper function to tryAddCollision
     def collisionValid(self):
         if (self.duration > 1) and (self.collision_vel != "-"):
             if (self.collision_vel < 150) and \
@@ -36,7 +37,9 @@ class Event:
     def updateSelf(self, distance, curr_time, curr_vel, curr_dxy, curr_wxy):
         self.duration += 1
         # self.collision_window_open and
-        if curr_time < self.collision_time + 2: # less than 2s since last updated coll time
+        if self.collision_window_open and \
+                not self.collision_found and \
+                curr_time < self.collision_time + 2:  # less than 2s since last updated coll time
             if TOO_CLOSE < distance < self.collision_dist:
                 self.collision_dist = distance
                 self.collision_vel = curr_vel
@@ -45,6 +48,24 @@ class Event:
                 self.collision_wxy = curr_wxy
                 print("updated distance to ", self.collision_dist, " at ", curr_time)
 
+    # determine if this event's collision is all set to record
+    def tryAddCollision(self, wrong_distances, all_collisions, dxy, clean):
+        if self.collision_vel != "-":
+            self.collision_vel = float(self.collision_vel) / 10 if wrong_distances \
+                else float(self.collision_vel)  # this is a very local change
+            if self.collisionValid():  # vel, distance, etc check
+                if not (is_redundant(all_collisions, (self.collision_time, self.wt_num,
+                                                      self.collision_wxy), wrong_distances)):
+                    # Create and record new collision
+
+                    disp = dist(dxy, self.collision_dxy) if not wrong_distances \
+                        else dist(dxy, self.collision_dxy) / 10
+                    collisionNew = Collision(self.collision_time, disp,
+                                             self.collision_vel,
+                                             clean, self.wt_num, self.collision_wxy,
+                                             self.def_num, self.duration,
+                                             self.collision_dist)
+                    return collisionNew
 
 
 # self.collision_vel = float(self.collision_vel) if not self.wrong_dists \
@@ -64,6 +85,9 @@ class Collision:
         self.distance = distance
 
     def printSelf(self):
+        print(mins(self.time), tuple((self.time, self.disp, self.vel, self.clean, self.wt_num,
+              self.wxy, self.def_num, self.duration, self.distance)))
+        '''
         print("just added, ",
             mins(self.time),
             self.vel,
@@ -73,4 +97,5 @@ class Collision:
             self.duration,
             self.distance
         )
+        '''
 
--- findCollisionsClasses.py
+++ findCollisionsClasses.py
@@ -75,28 +75,19 @@ def find_collisions(wt_num, def_num, csvs_path, wrong_distances, def_start, all_
             if distance <= PROXIMITY_DISTANCE:  # within significant proximity to deformed
                 if (not already_in_proximity) and (distance > TOO_CLOSE):  # new proximity event to follow
                     print(mins(curr_time), distance, "new event")
-                    # ddeal with existing event
-                    # then make new one
+
                     # Close any ongoing proximity event, record collision if it is valid
                     if len(WTevents) != 0:
                         # currEvent is the last event in WTevents
                         if not currEvent.timed_out:
-                            if currEvent.collision_vel != "-":
-                                currEvent.collision_vel = float(currEvent.collision_vel) / 10 if wrong_distances \
-                                    else float(currEvent.collision_vel)
-                                if currEvent.collisionValid():  # vel, distance, etc check
-                                    if not (is_redundant(all_collisions, (currEvent.collision_time, wt_num,
-                                                                          currEvent.collision_wxy), wrong_distances)):
-                                        # Create and record new collision
-                                        disp = dist(dxy, currEvent.collision_dxy) if not wrong_distances \
-                                            else dist(dxy, currEvent.collision_dxy) / 10
-                                        collisionNew = Collision(currEvent.collision_time, disp,
-                                                                 currEvent.collision_vel,
-                                                                 False, currEvent.wt_num, currEvent.collision_wxy,
-                                                                 currEvent.def_num, currEvent.duration,
-                                                                 currEvent.collision_dist)
-                                        all_collisions.append(collisionNew)
-                                        collisionNew.printSelf()
+                            collisionNew = currEvent.tryAddCollision(wrong_distances, all_collisions, dxy, False)
+                            if collisionNew is not None:
+                                all_collisions.append(collisionNew)
+                                collisionNew.printSelf()
+
+                                currEvent.collision_found = True
+                                WTevents[-1] = currEvent
+                                print("added new via end of prox")
 
                     # Create and add new event
                     currEvent = Event(curr_time, distance, wrow[8], dxy, wxy, def_num, wt_num)
@@ -109,50 +100,78 @@ def find_collisions(wt_num, def_num, csvs_path, wrong_distances, def_start, all_
                 # Not a new event, but the next timepoint in an ongoing event
                 elif already_in_proximity:
                     print(mins(curr_time), distance, "continuing")
-                    currEvent.collision_window_open = True if distance <= NECESSARY_DISTANCE else False
+                    # if leaving the necessary collision distance right now,
+                    # check if there was a legit collision in this window (if we havent found it yet)
+                    if currEvent.collision_window_open and distance >= NECESSARY_DISTANCE \
+                            and not currEvent.collision_found:
+                        collisionNew = currEvent.tryAddCollision(wrong_distances, all_collisions, dxy, False)
+                        # TODO does this negate "clean" collisions?
+                        if collisionNew is not None:  # valid collision found in the past window
+                            currEvent.collision_found = True
+                            currEvent.collision_window_open = False
+                            WTevents[-1] = currEvent
+                            print("collision has been ID'd and saved now")
+                    
+                    currEvent.collision_window_open = True if distance <= NECESSARY_DISTANCE and \
+                                                            not currEvent.collision_found else False
                     currEvent.updateSelf(distance, curr_time, wrow[8], dxy, wxy)
                     WTevents[-1] = currEvent  # update complete list
 
-            else:  # not in proximity
+            else:  # not in proximity, event should e ending/over
                 # print("not in proximity")
+                if already_in_proximity:  # end of proximity event, should record now # TODO wait for two timepoints after leaving?
+                    collisionNew = currEvent.tryAddCollision(wrong_distances, all_collisions, dxy, True)
+                    if collisionNew is not None:
+                        all_collisions.append(collisionNew)
+                        collisionNew.printSelf()
+
+                        currEvent.collision_found = True
+                        WTevents[-1] = currEvent
+                        print("added new via end of prox")
                 already_in_proximity = False  # will end proximity event
+
 
             # Calculate displacements after 2s
             # this is a problem because just because there's A smallest dist doesn't mean it'll be THE smallest dist?
             # is it likely that the real collision will happen more than 3s after the proximity begins
             # collision is smallest dist within first 3s?
 
-            if len(WTevents) != 0:  # if there is an existing collision on record # TODO is this a good feature
-                if (curr_time >= currEvent.collision_time + 5) and (not currEvent.timed_out):
-                    print(mins(curr_time), "doing the timeout thing")
-                    # if at least 5 seconds since collision passed and displacement not yet calculated
-                    #  TODO make global constants for time and other stuff lol
-                    # problem is that displacement can get off maybe if it is hit by another WT in this time
-                    #    -- drift does not close based on ANY other collision happening in that time :/
-                    #    and can't really be done because these are not calcd in order
-                    if currEvent.collision_vel != "-":
-                        currEvent.collision_vel = float(currEvent.collision_vel) / 10 if wrong_distances \
-                            else float(currEvent.collision_vel)
-                        if currEvent.collisionValid():  # vel, distance, etc check
-                            if not (is_redundant(all_collisions, (currEvent.collision_time, wt_num,
-                                                                  currEvent.collision_wxy), wrong_distances)):
-                                # Create and record new collision
-                                disp = dist(dxy, currEvent.collision_dxy) if not wrong_distances \
-                                    else dist(dxy, currEvent.collision_dxy) / 10
-                                collisionNew = Collision(currEvent.collision_time, disp,
-                                                         currEvent.collision_vel,
-                                                         True, currEvent.wt_num, currEvent.collision_wxy,
-                                                         currEvent.def_num, currEvent.duration,
-                                                         currEvent.collision_dist)
-                                all_collisions.append(collisionNew)
-                                collisionNew.printSelf()
-
-                    # True is because it's a clean collision (2s displacement after)
-                    # Though like .. could be not clean at all bc who knows if a diff WT hit the deformed and the
-                    #   "displacement" we record for it is due to the other WT's collision
-                    currEvent.timed_out = True
+            # if len(WTevents) != 0:  # if there is an existing collision on record # TODO is this a good feature
+            #     if (curr_time >= currEvent.collision_time + 5) and (not currEvent.timed_out):
+            #         print(mins(curr_time), "doing the timeout thing")
+            #         # if at least 5 seconds since collision passed and displacement not yet calculated
+            #         #  TODO make global constants for time and other stuff lol
+            #         # problem is that displacement can get off maybe if it is hit by another WT in this time
+            #         #    -- drift does not close based on ANY other collision happening in that time :/
+            #         #    and can't really be done because these are not calcd in order
+            #         if currEvent.collision_vel != "-":
+            #             currEvent.collision_vel = float(currEvent.collision_vel) / 10 if wrong_distances \
+            #                 else float(currEvent.collision_vel)
+            #             if currEvent.collisionValid():  # vel, distance, etc check
+            #                 if not (is_redundant(all_collisions, (currEvent.collision_time, wt_num,
+            #                                                       currEvent.collision_wxy), wrong_distances)):
+            #                     # Create and record new collision
+            #                     # print("FUCKO")
+            #
+            #                     disp = dist(dxy, currEvent.collision_dxy) if not wrong_distances \
+            #                         else dist(dxy, currEvent.collision_dxy) / 10
+            #                     collisionNew = Collision(currEvent.collision_time, disp,
+            #                                              currEvent.collision_vel,
+            #                                              True, currEvent.wt_num, currEvent.collision_wxy,
+            #                                              currEvent.def_num, currEvent.duration,
+            #                                              currEvent.collision_dist)
+            #                     all_collisions.append(collisionNew)
+            #                     collisionNew.printSelf()
+            #                     print("added new via timeout")
+            #
+            #
+            #         # True is because it's a clean collision (2s displacement after)
+            #         # Though like .. could be not clean at all bc who knows if a diff WT hit the deformed and the
+            #         #   "displacement" we record for it is due to the other WT's collision
+            #         currEvent.timed_out = True
             last_dxy = dxy
         last_time = curr_time
+        # TODO check for last collision?
     time_with_def = last_time - def_start  # i added this new - check if it works right?
     print("END OF WT ", wt_num, " :: ", len(all_collisions))
     return all_collisions, time_with_def
